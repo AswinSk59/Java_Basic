5fe7ea2cc79f1d9aa5b1563ecc6b294298804308
AswinSk59 <145536581+AswinSk59@users.noreply.github.com>
2024-04-17 09:25:45 +0530

    Try-catch NegativeArraySizeException

---
+++ ArrstrEx.java
@@ -0,0 +1,10 @@
+class ArrstrEx{
+	public static void main(String[]args){
+		try{
+			int[]a=new int[-5];
+		}
+		catch(NegativeArraySizeException e){
+			System.out.println("Array is -ve "+e);
+		}
+	}
+}
