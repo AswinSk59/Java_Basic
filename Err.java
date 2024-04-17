2c3d6499b6972cc9be1d0e6569f44fbbec9d441d
AswinSk59 <145536581+AswinSk59@users.noreply.github.com>
2024-04-17 09:24:36 +0530

    Try-Catch ArithmeticException

---
+++ Err.java
@@ -0,0 +1,14 @@
+class Err{
+	public static void main(String[]args){
+		int a=10,b=5,c=5;
+		int x,y;
+		try{
+			x=a/(b-c);
+		}
+		catch(ArithmeticException e){
+			System.out.println("Division by zero "+e);
+		}
+		y=a/(b+c);
+		System.out.println("y="+y);
+	}
+}
